60c10a3960bfcaa2b69a024e74fbdb5e4b03daad
shinopai <harbor.shinozaki@gmail.com>
2022-02-01 04:36:30 +0000

    set request domain per environment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins origins Rails.application.config.x.request['domain']
+    origins Rails.application.config.x.request['domain']
 
     resource '*',
       headers: :any,
